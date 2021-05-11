e0090d264cea1458a0d72b0b969bec7f790ba190
Kauan-Azevedo <57365069+Kauan-Azevedo@users.noreply.github.com>
2021-05-11 17:16:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,27 +5,25 @@ function salvarLocal() {
     var nomeI = document.getElementById("nome").value;
     var emailI = document.getElementById("email").value;
 
-    var usuariosJSON = JSON.parse(localStorage.getItem('jogador'))
+    var usuariosJSON = JSON.parse(localStorage.getItem('usuario'))
     if (usuariosJSON != null) {
-        usuarios = usuariosJSON
+        usuarios = usuariosJSON;
         usuarios.push({
             id: idI,
             nome: nomeI,
             email: emailI
-        })
+        });
         localStorage.setItem("usuario", JSON.stringify(usuarios))
     } else {
         usuarios.push({
             id: idI,
             nome: nomeI,
             email: emailI
-        })
-        localStorage.setItem("usuario", JSON.stringify(usuarios))
+        });
+        localStorage.setItem("usuario", JSON.stringify(usuarios));
     }
-    function usuCadastros(){
-        var json = JSON.parse(localStorage.getItem('usuario'))
-        json.sort((function (b,a){
-            return a.id-b.id
-        }))
-    }
+}
+
+function usuCadastros() {
+    var y = JSON.parse()
 }
