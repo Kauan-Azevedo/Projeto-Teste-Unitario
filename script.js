a3457bc352fe375988af543007c0de0bae80fb77
Kauan-Azevedo <57365069+Kauan-Azevedo@users.noreply.github.com>
2021-05-13 18:47:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 function salvarForm() {
-    if (typeof (Storage) !== "undefined") {
-        if (localStorage.cont) {
-            localStorage.cont = Number(localStorage.cont) + 1;
-        } else {
-            localStorage.cont = 1;
+    var nomeCampo = document.getElementById("nome").value;
+    var nomeEmail = document.getElementById("email").value;
+
+    if (nomeCampo == '' || nomeEmail == '') {
+        alert("Preencha todos os campos")
+    } else {
+        if (typeof (Storage) !== "undefined") {
+            if (localStorage.cont) {
+                localStorage.cont = Number(localStorage.cont) + 1;
+            } else {
+                localStorage.cont = 1;
+            }
+            var cad = document.getElementById('nome').value + " ; " + document.getElementById('email').value;
+            localStorage.setItem("usuario" + localStorage.cont, cad)
         }
-        var cad = document.getElementById('nome').value + " ; " + document.getElementById('email').value;
-        localStorage.setItem("usuario" + localStorage.cont, cad)
+        document.getElementById("nome").value = "";
+        document.getElementById("email").value = "";
     }
-    document.getElementById("nome").value = "";
-    document.getElementById("email").value = "";
 }
+if(window.localStorage.length <= 0){
+    alert("Olá bem-vindo \n após enviar reiniciar a pagina")
+}
+else{
 for (var cont = 1; cont <= localStorage.cont; cont++) {
     var salvos = localStorage.getItem("usuario" + cont);
-    document.getElementById("cadastros-sus").innerHTML += "Nome: " + salvos.split(";")[0] + ", " + "Email: " + salvos.split(";")[1] + "<br>";
+    document.getElementById("cadastros-sus").innerHTML += "Nome: " + salvos.split(";")[0] + "<br> " + "Email: " + salvos.split(";")[1] + "<br><br>";
+}
 }
