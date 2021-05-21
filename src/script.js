b0c86ce2a8dfd022d1e5592c1c3b871a9d6f707d
Kauan-Azevedo <57365069+Kauan-Azevedo@users.noreply.github.com>
2021-05-21 16:51:06 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function Cadastro() {
                 localStorage.cont = 1;
             }
 
-            var cad = nomeCampo + " ; " + nomeEmail //document.getElementById('nome').value + " ; " + document.getElementById('email').value;
+            var cad = this.nomeCampo + " ; " + this.nomeEmail;
             localStorage.setItem("usuario" + localStorage.cont, cad);
         },
         pegaNome: function () {
@@ -22,6 +22,43 @@ function Cadastro() {
         pegaEmail: function () {
             return nomeEmail
         }
+
     }
     return salvarUsu;
 }
+
+/*
+function salvarForm() {
+
+    nomeCampo = "Carlos";    //document.getElementById("nome").value;
+    nomeEmail = "CarlosAntonio@gmail.com"; //document.getElementById("email").value;
+
+   if (nomeCampo == '' || nomeEmail == '') {
+        alert("Preencha todos os campos")
+    } else {
+        if (typeof (Storage) !== "undefined") {
+            if (localStorage.cont) {
+                localStorage.cont = Number(localStorage.cont) + 1;
+           } else {
+                localStorage.cont = 1;
+            }
+
+            var cad = nomeCampo + " ; " + nomeEmail //document.getElementById('nome').value + " ; " + document.getElementById('email').value;
+            localStorage.setItem("usuario" + localStorage.cont, cad);
+        }
+
+document.getElementById("nome").value = "";
+document.getElementById("email").value = "";
+    }
+location.reload();
+}
+if (window.localStorage.length <= 0) {
+    alert("Olá bem-vindo")
+}
+else {
+    for (var cont = 1; cont <= localStorage.cont; cont++) {
+        var salvos = localStorage.getItem("usuario" + cont);
+        document.getElementById("cadastros-sus").innerHTML = "Nome: " + salvos.split(";")[0] + "<br> " + "Email: " + salvos.split(";")[1] + "<br><br>";
+    }
+}
+*/
